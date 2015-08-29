a627a753f6244b5c28e79d4950624ea249d02b8f
Benjamen Meyer <bm_witness@yahoo.com>
2015-08-29 01:43:16 -0400

    Bug Fix: PEP8/spacing

--- stackinabox/services/hello/__init__.py
+++ stackinabox/services/hello/__init__.py
@@ -5,16 +5,16 @@ from stackinabox.services.service import StackInABoxService
 
 
 class HelloService(StackInABoxService):
-	"""Example Hello World Stack-In-A-Box Service application
-	"""
+    """Example Hello World Stack-In-A-Box Service application
+    """
 
     def __init__(self):
-		"""Initialize and register the single end-point
-		"""
+        """Initialize and register the single end-point
+        """
         super(HelloService, self).__init__('hello')
         self.register(StackInABoxService.GET, '/', HelloService.handler)
 
     def handler(self, request, uri, headers):
-		"""Single End-Point that simply returns 'hello'
-		"""
+        """Single End-Point that simply returns 'hello'
+        """
         return (200, headers, 'Hello')
